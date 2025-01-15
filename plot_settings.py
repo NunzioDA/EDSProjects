c42f5286280e8e23f08cc6870ebe80146f886fbd
NunzioDA <nunziodamore99@gmail.com>
2025-01-15 15:15:56 +0100

    First commit plot_settings.py

---
+++ plot_settings.py
@@ -0,0 +1,36 @@
+
+from matplotlib import pyplot as plt
+
+
+colors = ['#C88C27',  '#5C368E', '#BD4946', '#487DBA',  '#97B953', '#ff7f0e']
+foreground = '#f7f7f7'
+background = '#1f1f1f'
+
+def title(title):
+    plt.title(title, color=foreground)
+
+def figure_ax():
+    fig = plt.figure(facecolor=background)
+
+    ax = fig.add_subplot(111)
+    ax.set_facecolor(background)
+    return fig, ax
+
+def color(Index):
+    if len(colors)>Index:
+        return colors[Index]
+    else:
+        return None
+
+def set_plot_theme(ax, xlabel='x', ylabel='y', show_legend=True):
+    ax.set_xlabel(xlabel, color=foreground,)
+    ax.set_ylabel(ylabel, color=foreground,)
+    ax.tick_params(axis='x', labelcolor=foreground, color=foreground)
+    ax.tick_params(axis='y', labelcolor=foreground, color=foreground)
+    ax.spines['top'].set_color(foreground)     
+    ax.spines['right'].set_color(foreground)    
+    ax.spines['bottom'].set_color(foreground) 
+    ax.spines['left'].set_color(foreground) 
+
+    if show_legend:
+        plt.legend(facecolor="#2f2f2f", labelcolor=foreground,edgecolor="none")
